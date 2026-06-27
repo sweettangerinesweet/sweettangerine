674f984218fb42b71c668843046330243bf17774
sweettangerinesweet <sweettangerinesweet39@gmail.com>
2026-06-27 16:23:38 +0700

    Load books from API

--- js/script.js
+++ js/script.js
@@ -1,1 +1,53 @@
-// Sweet Tangerine JavaScript
+// ======================================
+// Sweet Tangerine
+// Main Script
+// ======================================
+
+let books = [];
+
+// saat website dibuka
+window.onload = async function () {
+
+    books = await getBooks();
+
+    console.log("Books loaded:", books.length);
+
+    renderBooks(books);
+
+};
+
+
+// menampilkan buku
+function renderBooks(bookList) {
+
+    const container = document.getElementById("bookContainer");
+
+    container.innerHTML = "";
+
+    bookList.forEach(book => {
+
+        container.innerHTML += `
+
+            <div class="book-card">
+
+                <img src="${book.cover}" alt="${book.title}">
+
+                <div class="book-info">
+
+                    <div class="book-title">
+                        ${book.title}
+                    </div>
+
+                    <div class="book-author">
+                        ${book.author}
+                    </div>
+
+                </div>
+
+            </div>
+
+        `;
+
+    });
+
+}
